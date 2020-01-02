82037c9cc5ac284214d0e4c1e3366789006138fe
Francielle Luz <francielle.luz@gft.com>
2020-01-02 09:16:39 -0300

    correção de numero magico

--- src/festaAniversario/observe/ChegadaAniversarianteEvent.java
+++ src/festaAniversario/observe/ChegadaAniversarianteEvent.java
@@ -5,11 +5,13 @@ import java.util.Date;
 public class ChegadaAniversarianteEvent {
 
 	private final Date horaDaChegada;
+
 	public ChegadaAniversarianteEvent(Date horaDaChegada) {
 		super();
 		this.horaDaChegada = horaDaChegada;
 	}
-	public  Date getHoraDaChegada() {
+
+	public Date getHoraDaChegada() {
 		return horaDaChegada;
 	}
 }
--- src/festaAniversario/observe/Porteiro.java
+++ src/festaAniversario/observe/Porteiro.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class Porteiro extends Thread {
 	private List<ChegadaAniversarianteObserver> observers = new ArrayList<ChegadaAniversarianteObserver>();
-
+	private final int CHEGOU =1;
 	public void addChegadaAniversarianteObserver(ChegadaAniversarianteObserver observer) {
 		this.observers.add(observer);
 	}
@@ -17,7 +17,7 @@ public class Porteiro extends Thread {
 		Scanner scanner = new Scanner(System.in);
 		while (true) {
 			int valor = scanner.nextInt();
-			if (valor == 1) {
+			if (valor == CHEGOU) {
 				ChegadaAniversarianteEvent event = new ChegadaAniversarianteEvent(new Date());
 				
 				this.observers.forEach(item ->{
